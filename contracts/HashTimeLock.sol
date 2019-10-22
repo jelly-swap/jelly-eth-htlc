3ce48ac34ac943f06e8837a4c246ebc145820bef
kraykov <raykov@weichain.tech>
2019-10-22 14:12:34 +0300

    Add solhint and prettier. Fix getStatus functionality.

--- contracts/HashTimeLock.sol
+++ contracts/HashTimeLock.sol
@@ -1,30 +1,19 @@
-pragma experimental ABIEncoderV2;
 pragma solidity >=0.5.0 <0.6.0;
+pragma experimental ABIEncoderV2;
 
 contract HashTimeLock {
+    mapping(bytes32 => LockContract) public contracts;
 
-    mapping (bytes32 => LockContract) contracts;
-
-    enum SwapStatus {
-        INVALID,
-        ACTIVE,
-        REFUNDED,
-        WITHDRAWN,
-        EXPIRED
-    }
+    enum SwapStatus {INVALID, ACTIVE, REFUNDED, WITHDRAWN, EXPIRED}
 
     struct LockContract {
         uint256 inputAmount;
         uint256 outputAmount;
         uint256 expiration;
-
         bytes32 hashLock;
-
         SwapStatus status;
-
         address payable sender;
         address payable receiver;
-
         string outputNetwork;
         string outputAddress;
     }
@@ -48,13 +37,10 @@ contract HashTimeLock {
         uint256 inputAmount,
         uint256 outputAmount,
         uint256 expiration,
-
         bytes32 indexed id,
         bytes32 hashLock,
-
         address indexed sender,
         address indexed receiver,
-
         string outputNetwork,
         string outputAddress
     );
@@ -62,8 +48,11 @@ contract HashTimeLock {
     modifier withdrawable(bytes32 id, bytes32 secret) {
         LockContract memory tempContract = contracts[id];
         require(tempContract.status == SwapStatus.ACTIVE, "SWAP_NOT_ACTIVE");
-        require(tempContract.expiration > block.timestamp,"INVALID_TIME");
-        require(tempContract.hashLock == sha256(abi.encodePacked(secret)),"INVALID_SECRET");
+        require(tempContract.expiration > block.timestamp, "INVALID_TIME");
+        require(
+            tempContract.hashLock == sha256(abi.encodePacked(secret)),
+            "INVALID_SECRET"
+        );
         _;
     }
 
@@ -82,10 +71,7 @@ contract HashTimeLock {
         address payable receiver,
         string memory outputNetwork,
         string memory outputAddress
-    )
-        public
-        payable
-    {
+    ) public payable {
         address payable sender = msg.sender;
         uint256 inputAmount = msg.value;
 
@@ -93,7 +79,15 @@ contract HashTimeLock {
 
         require(inputAmount > 0, "INVALID_AMOUNT");
 
-        bytes32 id = sha256(abi.encodePacked(sender, receiver, inputAmount, hashLock, expiration));
+        bytes32 id = sha256(
+            abi.encodePacked(
+                sender,
+                receiver,
+                inputAmount,
+                hashLock,
+                expiration
+            )
+        );
 
         contracts[id] = LockContract(
             inputAmount,
@@ -132,11 +126,7 @@ contract HashTimeLock {
         return true;
     }
 
-    function refund(bytes32 id)
-        external
-        refundable(id)
-        returns (bool)
-    {
+    function refund(bytes32 id) external refundable(id) returns (bool) {
         LockContract storage c = contracts[id];
         c.status = SwapStatus.REFUNDED;
         c.sender.transfer(c.inputAmount);
@@ -144,36 +134,39 @@ contract HashTimeLock {
         return true;
     }
 
-    function getContract(bytes32 id)
-        public
-        view
-        returns (LockContract memory)
-    {
+    function getContract(bytes32 id) public view returns (LockContract memory) {
         LockContract memory c = contracts[id];
         return c;
     }
 
-    function contractExists(bytes32 id)
-        public
-        view
-        returns (bool)
-    {
+    function contractExists(bytes32 id) public view returns (bool) {
         return contracts[id].status != SwapStatus.INVALID;
     }
 
     function getStatus(bytes32[] memory ids)
         public
         view
-        returns(SwapStatus[] memory result)
+        returns (uint8[] memory)
     {
+        uint8[] memory result = new uint8[](ids.length);
+
         for (uint256 index = 0; index < ids.length; index++) {
-            LockContract memory tempContract = contracts[ids[index]];
-
-            if(tempContract.status == SwapStatus.ACTIVE && tempContract.expiration < block.timestamp) {
-                result[index] = SwapStatus.EXPIRED;
-            }
-
-            result[index] = tempContract.status;
+            result[index] = getStatus(ids[index]);
         }
+
+        return result;
+    }
+
+    function getStatus(bytes32 id) public view returns (uint8 result) {
+        LockContract memory tempContract = contracts[id];
+
+        if (
+            tempContract.status == SwapStatus.ACTIVE &&
+            tempContract.expiration < block.timestamp
+        ) {
+            result = uint8(SwapStatus.EXPIRED);
+        }
+
+        result = uint8(tempContract.status);
     }
 }
