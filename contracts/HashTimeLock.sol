8708e0b224ef0058f560d4d746de9d88188c0b86
kraykov <raykov@weichain.tech>
2020-03-12 19:05:03 +0200

    1) Make status constants public so they can be used by external contract. 2) Change return of getStatus to uint256

--- contracts/HashTimeLock.sol
+++ contracts/HashTimeLock.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.5.0​;
+pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;
 
 contract HashTimeLock {
@@ -9,11 +9,11 @@ contract HashTimeLock {
     // INVALID - ACTIVE |
     //                   \ - EXPIRED - REFUNDED
 
-    uint256 private constant INVALID = 0; // Uninitialized  swap -> can go to ACTIVE
-    uint256 private constant ACTIVE = 1; // Active swap -> can go to WITHDRAWN or EXPIRED
-    uint256 private constant REFUNDED = 2; // Swap is refunded -> final state.
-    uint256 private constant WITHDRAWN = 3; // Swap is withdrawn -> final state.
-    uint256 private constant EXPIRED = 4; // Swap is expired -> can go to REFUNDED
+    uint256 public constant INVALID = 0; // Uninitialized  swap -> can go to ACTIVE
+    uint256 public constant ACTIVE = 1; // Active swap -> can go to WITHDRAWN or EXPIRED
+    uint256 public constant REFUNDED = 2; // Swap is refunded -> final state.
+    uint256 public constant WITHDRAWN = 3; // Swap is withdrawn -> final state.
+    uint256 public constant EXPIRED = 4; // Swap is expired -> can go to REFUNDED
 
     struct LockContract {
         uint256 inputAmount;
@@ -59,8 +59,8 @@ contract HashTimeLock {
         uint256 expiration,
         bytes32 hashLock,
         address payable receiver,
-        string memory outputNetwork,
-        string memory outputAddress
+        string calldata outputNetwork,
+        string calldata outputAddress
     ) external payable {
         address payable sender = msg.sender;
         uint256 inputAmount = msg.value;
@@ -70,7 +70,7 @@ contract HashTimeLock {
         require(inputAmount > 0, 'INVALID_AMOUNT');
 
         bytes32 id = sha256(
-          abi.encodePacked(sender, receiver, inputAmount, hashLock, expiration)
+            abi.encodePacked(sender, receiver, inputAmount, hashLock, expiration)
         );
 
         require(contracts[id].status == INVALID, "SWAP_EXISTS");
@@ -130,8 +130,8 @@ contract HashTimeLock {
         emit Refund(id, c.hashLock, c.sender, c.receiver);
     }
 
-    function getStatus(bytes32[] memory ids) public view returns (uint8[] memory) {
-        uint8[] memory result = new uint8[](ids.length);
+    function getStatus(bytes32[] memory ids) public view returns (uint256[] memory) {
+        uint256[] memory result = new uint256[](ids.length);
 
         for (uint256 index = 0; index < ids.length; index++) {
             result[index] = getSingleStatus(ids[index]);
@@ -140,7 +140,7 @@ contract HashTimeLock {
         return result;
     }
 
-    function getSingleStatus(bytes32 id) public view returns (uint8 result) {
+    function getSingleStatus(bytes32 id) public view returns (uint256 result) {
         LockContract memory tempContract = contracts[id];
 
         if (
